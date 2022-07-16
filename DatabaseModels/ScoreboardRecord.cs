4932175e74a82a0efb53b2a731ba7480531d0b36
cuqmbr <dr.juniorf@gmail.com>
2022-07-16 10:58:56 +0300

    fix: make username property not nullable

--- DatabaseModels/ScoreboardRecord.cs
+++ DatabaseModels/ScoreboardRecord.cs
@@ -5,7 +5,7 @@ namespace DatabaseModels;
 public class ScoreboardRecord
 {
     [Key]
-    public string? Username { get; set; }
+    public string Username { get; set; }
     public int Score { get; set; }
     
     public DateTime PostTime { get; set; }
